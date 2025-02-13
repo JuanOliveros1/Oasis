e2fdc8b6e7a9f8173686082ed20c9b0fb43d8d7e
Ezra Carrillo <ezra0522@icloud.com>
2025-02-12 19:52:21 -0600

    Initial commit

---
+++ Oasis/OasisApp.swift
@@ -0,0 +1,20 @@
+//
+//  OasisApp.swift
+//  Oasis
+//
+//  Created by Ezra Carrillo on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OasisApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ OasisTests/OasisTests.swift
@@ -0,0 +1,17 @@
+//
+//  OasisTests.swift
+//  OasisTests
+//
+//  Created by Ezra Carrillo on 2/12/25.
+//
+
+import Testing
+@testable import Oasis
+
+struct OasisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
